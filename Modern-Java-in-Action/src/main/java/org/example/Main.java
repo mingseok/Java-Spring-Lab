2609f5f0829e0371fe9ea8521aa468b69eacf0e3
minseok <alstjr706@naver.com>
2023-01-13 22:06:54 +0900

    git : 깃 테스트

--- Modern-Java-in-Action/src/main/java/org/example/Main.java
+++ Modern-Java-in-Action/src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ㅇㅇㅇㅇㅇㅇHello world!");
+        System.out.println("ddddddHello world!");
     }
 }
