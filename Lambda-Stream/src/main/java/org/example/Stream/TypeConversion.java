e7c77aba86464b1716b713ec08b1f5ed9c66842d
minseok <alstjr706@naver.com>
2023-02-08 09:10:56 +0900

    feat : (Lambda-Stream) 스트림 형변환

---
+++ Lambda-Stream/src/main/java/org/example/Stream/TypeConversion.java
@@ -0,0 +1,17 @@
+package org.example.Stream;
+
+import java.util.function.Function;
+
+public class TypeConversion {
+    public static void main(String[] args) {
+        Function<String, Integer> number1 = (String s) -> Integer.parseInt(s);
+
+        // 위 코드를 메서드 참조로 바꾸면 이렇게 된다.
+        // 클래스이름::메서드이름;
+        // 클래스이름 다시 말해 뭐다? -> 타입이다.
+        Function<String, Integer> number2 = Integer::parseInt;
+
+        System.out.println(number1.apply("100") + 200);
+    }
+}
+
