31ebeb14755ea6e10b48e5cda68ab9a83240d855
minseok <alstjr706@naver.com>
2023-02-15 06:25:48 +0900

    feat : (java-8) 함수형 인터페이스 사용 예제

--- Java-8/src/main/java/org/example/Foo.java
+++ Java-8/src/main/java/org/example/Foo.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import java.sql.SQLOutput;
+import java.util.List;
+import java.util.function.Consumer;
+
 @FunctionalInterface
 interface RunSomething {
     void doIt();
@@ -9,6 +13,9 @@ public class Foo {
     public static void main(String[] args) {
         RunSomething runSomething = () -> System.out.println("hello");
         runSomething.doIt();
+
+        Consumer<Integer> printT = (i) -> System.out.println(i);
+        printT.accept(10); // T타입을 받아서 넣기만 할수 있다. 리턴 할 수 없음
     }
 }
 
