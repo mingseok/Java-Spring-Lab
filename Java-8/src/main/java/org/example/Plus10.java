b4c4e515ec508852ce4b6a352633d871804c1b87
minseok <alstjr706@naver.com>
2023-02-15 00:07:42 +0900

    feat : (Lambda-Stream) compose(), andThen() 사용 예제

--- Java-8/src/main/java/org/example/Plus10.java
+++ Java-8/src/main/java/org/example/Plus10.java
@@ -17,14 +17,25 @@ class Execution {
         // Plus10 plus10 = new Plus10();
         // System.out.println(plus10.apply(1));
 
+
         Function<Integer, Integer> plus10 = (i) -> i + 10;
-        //System.out.println(plus10.apply(1));
+        System.out.println(plus10.apply(1)); // 11
+
 
         Function<Integer, Integer> multiply2 = (i) -> i * 2;
-        // System.out.println(multiply2.apply(1));
+        System.out.println(multiply2.apply(1)); // 2
 
-        //TODO: 컴포즈를 사용하면 어떻게 될까?
-        plus10.compose(multiply2);
 
+        // 컴포즈를 사용하면 어떻게 될까? (=힌드틑 함수가 두개 있다고 생각하면 된다)
+        // 첫번째. 어떤 값('2')가 들어오면 compose() 괄호 안에 있는 것부터하여
+        // 들어온 값에 *2 를 진행한다. 그다음 compose() 앞부분에 있는 plus10 함수를 진행 시키는 것이다.
+        // 그렇게 된다면, +10을 계산하게 되어 14가 출력 되는 것이다.
+        Function<Integer, Integer> multiply2AndPlus10 = plus10.compose(multiply2);
+        System.out.println(multiply2AndPlus10.apply(2)); // 14
+
+
+        // andThen()은 compose() 메서드의 반대이다.
+        // 즉, plus10부터 진행한다는 것이다.
+        System.out.println(plus10.andThen(multiply2).apply(2));
     }
 }
