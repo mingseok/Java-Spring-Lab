d3aa855bfc755344476e8836b31c97a888bc3ece
Minseok Kim <alstjr706@naver.com>
2025-02-09 22:09:15 +0900

    파일 제거.

--- Objects/src/main/java/object/movie/DiscountCondition.java
+++
@@ -1,5 +0,0 @@
-package object.movie;
-
-public interface DiscountCondition {
-    boolean isSatisfiedBy(Screening screening);
-}
--- Objects/src/main/java/object/ticket/Invitation.java
+++
@@ -1,8 +0,0 @@
-package object.ticket;
-
-import java.time.LocalDate;
-
-// 초대장이라는 개념을 구현한 Invitation 클래스
-public class Invitation {
-    private LocalDate when;
-}
